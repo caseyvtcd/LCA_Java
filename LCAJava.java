57c07d8448796ebd3c399fd9b778c42053a33c42
vincecasey <vincecasey@users.noreply.github.com>
2020-11-19 17:58:06 +0000

    New Implementation of LCA

--- LCAJava.java
+++ LCAJava.java
@@ -1,44 +1,130 @@
-public class LCAJava
+// Data structure to store a Binary Tree node
+class Node
 {
-    class Solution {
+	int data;
+	Node left = null, right = null;
 
-        private TreeNode ans;
+	Node(int data) {
+		this.data = data;
+	}
+}
 
-        public Solution() {
-            // Variable to store LCA node.
-            this.ans = null;
-        }
+class LCAJava
+{
+	// Wrapper over Node class
+	static class NodeWrapper {
+		public Node node;
 
-        private boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
+		NodeWrapper(Node node) {
+			this.node = node;
+		}
+	}
 
-            // If reached the end of a branch, return false.
-            if (currentNode == null) {
-                return false;
-            }
+	// Function to check if given node is present in binary tree or not
+	public static boolean isNodePresent(Node root, Node node)
+	{
+		// base case
+		if (root == null) {
+			return false;
+		}
 
-            // Left Recursion. If left recursion returns true, set left = 1 else 0
-            int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
+		// if node is found, return true
+		if (root == node) {
+			return true;
+		}
 
-            // Right Recursion
-            int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
+		// return true if node is found in the left subtree or right subtree
+		return isNodePresent(root.left, node) || isNodePresent(root.right, node);
+	}
 
-            // If the current node is one of p or q
-            int mid = (currentNode == p || currentNode == q) ? 1 : 0;
+	// Function to find lowest common ancestor of given nodes x and y where
+	// both x and y are present in the binary tree.
+	// The function returns true if x or y is found in subtree rooted at root
+	// lca -> stores LCA(x, y)
+	public static boolean findLCA(Node root, NodeWrapper lca, Node x, Node y)
+	{
+		// base case 1: return false if tree is empty
+		if (root == null) {
+			return false;
+		}
 
+		// base case 2: return true if either x or y is found
+		if (root == x || root == y)
+		{
+			// set lca to current node
+			lca.node = root;
+			return true;
+		}
 
-            // If any two of the flags left, right or mid become True
-            if (mid + left + right >= 2) {
-                this.ans = currentNode;
-            }
+		// recursively check if x or y exists in the left subtree
+		boolean left = findLCA(root.left, lca, x, y);
 
-            // Return true if any one of the three bool values is True.
-            return (mid + left + right > 0);
-        }
+		// recursively check if x or y exists in the right subtree
+		boolean right = findLCA(root.right, lca, x, y);
 
-        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-            // Traverse the tree
-            this.recurseTree(root, p, q);
-            return this.ans;
-        }
-    }
+		// if x is found in one subtree and y is found in other subtree,
+		// update lca to current node
+		if (left && right) {
+			lca.node = root;
+		}
+
+		// return true if x or y is found in either left or right subtree
+		return left || right;
+	}
+
+	// Function to find lowest common ancestor of nodes x and y
+	public static void findLCA(Node root, Node x, Node y)
+	{
+		// lca stores lowest common ancestor
+		Node lca = null;
+
+		// Wrap lca node so it's reference can be changed inside findLCA() method
+		NodeWrapper LCA = new NodeWrapper(lca);
+
+		// call LCA procedure only if both x and y are present in the tree
+		if (isNodePresent(root, y) && isNodePresent(root, x)) {
+			findLCA(root, LCA, x, y);
+			lca = LCA.node;
+		}
+
+		// if LCA exists, print it
+		if (lca != null) {
+			System.out.println("LCA is " + lca.data);
+		} else {
+			System.out.print("LCA do not exist\n");
+		}
+	}
+
+	public static void main(String[] args)
+	{
+	    /* Construct below tree
+	          1
+	        /   \
+	       /     \
+	      2          3
+	       \     / \
+	        4   5   6
+	           / \
+	          7   8
+	    */
+
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.right = new Node(4);
+		root.right.left = new Node(5);
+		root.right.right = new Node(6);
+		root.right.left.left = new Node(7);
+		root.right.right.right = new Node(8);
+		root.right.right.right.left = new Node(9);
+
+		findLCA(root, root.right.left.left, root.right.right);
+		findLCA(root, root.right.left.left, new Node(10));
+		findLCA(root, root.right.left.left, root.right.left.left);
+		findLCA(root, root.right.left.left, root.right.left);
+		findLCA(root, root.left, root.right.left);
+
+        
+
+	}
 }
